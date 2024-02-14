7ef1d71b7832d36764106c746c894995f0319630
23-Pravallika <learngain23@gmail.com>
2024-02-14 18:30:48 +0530

    AutoPush

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,1 +1,3 @@
 AZ = ["us-east-1a" , "us-east-1b"]
+DEFAULT_VPC_ID = "vpc-04ef1edd34bd5aafa"
+DEFAULT_VPC_CIDR = "172.31.0.0/16"
--- vars.tf
+++ vars.tf
@@ -3,3 +3,5 @@ variable "AZ" {}
 variable "ENV" {}
 variable "PUBLIC_SUBNET" {}
 variable "PRIVATE_SUBNET" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
